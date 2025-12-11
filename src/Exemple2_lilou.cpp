59355c24ee529a5d0136f8c9dcc7de47df670cbc
Lilou <lilougrimaud.13@gmail.com>
2025-12-11 13:20:25 +0100

    Exemple2_Lilou version qui marche (boutton interactif...)

--- src/Exemple2_lilou.cpp
+++ src/Exemple2_lilou.cpp
@@ -1,19 +1,3 @@
-/*//a mettre dans le main
-#include <SDL.h>
-#include <SDL_image.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <math.h>
-
-//Cette partie définie les statistique de la fenêtre, le maximum de chaque classe liée a l'animal
-#define WINDOW_WIDTH 400
-#define WINDOW_HEIGHT 500
-#define MAX_STAT 100
-#define STAT_DECAY_RATE 0.5f //ici c'est le temps de diminution des statistiques pour les animaux
-
-*/
-
 #include <SDL.h>
 #include <SDL_image.h>
 #include <iostream>
@@ -37,18 +21,19 @@ void Barre_Etat(SDL_Renderer* renderer, int x, int y, int w, int h, float percen
     SDL_RenderDrawRect(renderer, &bg);
 }
 
-    //Initialisation SDL_image et SDL
+//Initialisation SDL_image et SDL
 bool Initialise_SDL(SDL_Window** win, SDL_Renderer** ren) {
     SDL_Init(SDL_INIT_VIDEO); //initialisation de la fenêtre
     IMG_Init(IMG_INIT_PNG); //et de l'image
 
     *win = SDL_CreateWindow("Super jeu de Léane et Lilou",
-                            100, 100, 600, 500, 0);
+        100, 100, 600, 500, 0);
 
     *ren = SDL_CreateRenderer(*win, -1, SDL_RENDERER_ACCELERATED); //Dessin des barres grace a ça 
 
     return true;
 }
+
 //vider la mémoire après fermeture de la fenêtre
 void cleanup(SDL_Window* win, SDL_Renderer* ren) {
     if (ren) SDL_DestroyRenderer(ren);
@@ -72,37 +57,89 @@ int main(int argc, char* argv[]) {
         "M",
         1,
         renderer,
-        "res/Chat1/Chat1_frame1.png"  
+        "res/Chat1/Chat1_frame1.png"
     );
 
     Stats stats;
 
     bool run = true;
-    SDL_Event e;
+    SDL_Event evenement;
+
+    //Dessin des boutons pour intéragir avec l'animal.(x, y, largeur, hauteur)
+    SDL_Rect btnNourrir = {60, 360, 80, 20};
+    SDL_Rect btnSoigner = {60, 390, 80, 20};
+
+    // Timer
+    static Uint32 lastTime = SDL_GetTicks();
 
     while (run) {
-        while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT) run = false;
-            if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE) run = false;
+
+        while (SDL_PollEvent(&evenement)) {
+            if (evenement.type == SDL_QUIT) run = false;
+            if (evenement.type == SDL_KEYDOWN && evenement.key.keysym.sym == SDLK_ESCAPE) run = false;
+
+            if (evenement.type == SDL_MOUSEBUTTONDOWN) {
+                int mx = evenement.button.x; //coordonée de la souris au moment du clic pour intéragir.
+                int my = evenement.button.y;
+
+                // Nourriture
+                if (mx >= btnNourrir.x && mx <= btnNourrir.x + btnNourrir.w &&
+                    my >= btnNourrir.y && my <= btnNourrir.y + btnNourrir.h) {
+                    stats.setFaim(stats.getFaim() + 25); //tout est ok alors ajouter +25 a la barre nourriture (Faim).
+                }
+
+                // Médicament
+                if (mx >= btnSoigner.x && mx <= btnSoigner.x + btnSoigner.w &&
+                    my >= btnSoigner.y && my <= btnSoigner.y + btnSoigner.h) {
+                    stats.setSante(stats.getSante() + 10);
+                }
+            }
         }
 
-        // Fond gris clair
+        // Fonction qui effacer l'écran
         SDL_SetRenderDrawColor(renderer, 230, 230, 230, 255);
         SDL_RenderClear(renderer);
 
-        //cadre autour des barre d'état
-        SDL_Rect cadre = {50, 350, 500, 90};  
+        // Création du cadre qui entoure les boutons et les barres 
+        SDL_Rect cadre = {50, 350, 500, 90};
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderDrawRect(renderer, &cadre);
 
-        // Barre d'état (x, y, largeur, hauteur)
-        Barre_Etat(renderer, 330, 360, 200, 10, stats.getFaim(),    {255,   0,   0, 255});
-        Barre_Etat(renderer, 330, 380, 200, 10, stats.getJoie(),    {  0, 255,   0, 255});
-        Barre_Etat(renderer, 330, 400, 200, 10, stats.getSante(),   {  0,   0, 255, 255});
-        Barre_Etat(renderer, 330, 420, 200, 10, stats.getEnergie(), {255, 255,   0, 255});
+        // Dessin des barres d'Etats
+        Barre_Etat(renderer, 330, 360, 200, 10, stats.getFaim(), {255, 0, 0, 255});
+        Barre_Etat(renderer, 330, 380, 200, 10, stats.getJoie(), {0, 255, 0, 255});
+        Barre_Etat(renderer, 330, 400, 200, 10, stats.getSante(), {0, 0, 255, 255});
+        Barre_Etat(renderer, 330, 420, 200, 10, stats.getEnergie(), {255, 255, 0, 255});
 
-        //Affichage 
+        // Affichage de l'animal
         monAnimal.render(renderer);
+
+        // Dessin des boutons 
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderFillRect(renderer, &btnNourrir);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderDrawRect(renderer, &btnNourrir);
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+        SDL_RenderFillRect(renderer, &btnSoigner);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderDrawRect(renderer, &btnSoigner);
+
+        // Timer
+        Uint32 currentTime = SDL_GetTicks();
+
+        if (currentTime - lastTime >= 1000) { // Toutes les 1 seconde via currentTime qui récupère l'heure du programme
+            stats.setFaim(stats.getFaim() - 1);
+            stats.setJoie(stats.getJoie() - 0.5f);
+            stats.setEnergie(stats.getEnergie() - 0.8f);
+
+            //Si la faim atteint 0 l'animal perd petit a petit de la santé.
+            if (stats.getFaim() <= 0) {
+                stats.setSante(stats.getSante() - 0.5f);
+            }
+
+            lastTime = currentTime;
+        }
 
         SDL_RenderPresent(renderer);
         SDL_Delay(16);
--- src/Statistique.cpp
+++ src/Statistique.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 
 // Constructeur
-Stats::Stats() : faim(100.0f), joie(50.0f), sante(100.0f), energie(100.0f) {} //le f a coter de 100.0 signifie float
+Stats::Stats() : faim(100.0f), joie(100.0f), sante(100.0f), energie(100.0f) {} //le f a coter de 100.0 signifie float
 
 // Getters
 float Stats::getFaim() const { return faim; }
@@ -11,7 +11,7 @@ float Stats::getSante() const { return sante; }
 float Stats::getEnergie() const { return energie; }
 
 // Setters
-void Stats::setFaim(float v) { faim = std::max(0.0f, std::min(v, 100.0f)); }
+void Stats::setFaim(float v) { faim = std::max(0.0f, std::min(v, 100.0f)); } //la faim ne peut pas dépasser 0 ni augmenter a + de 100
 void Stats::setJoie(float v) { joie = std::max(0.0f, std::min(v, 100.0f)); }
 void Stats::setSante(float v) { sante = std::max(0.0f, std::min(v, 100.0f)); }
 void Stats::setEnergie(float v) { energie = std::max(0.0f, std::min(v, 100.0f)); }
